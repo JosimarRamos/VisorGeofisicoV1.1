86ec7f1a8ab49588f9b180f70555f65d3966836d
jmj-1000 <127218473+jmj-1000@users.noreply.github.com>
2026-07-13 18:45:16 -0400

    Modificado para el cache y uso sin conexion

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'visor-cache-v1';
+const PRECACHE = [
+  'shared/css/visor-styles.css',
+  'shared/js/three.min.js',
+  'shared/js/OrbitControls.js',
+  'shared/js/visor-core.js'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
